112061d07e3d6a659c2bf04c8381b5fa282b72ae
unknown <chris.a.desilva@gmail.com>
2019-06-03 18:19:15 -0500

    Update env variable name for places, add sample env for open source contributions

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,7 +7,7 @@ import './Map.css';
 import FindACourt from "./FindACourt"
 
 const MyLocation = () => <Icon circular inverted color="teal" name="map pin" />;
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+const API_KEY = `${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`;
 
 class Map extends Component {
   static defaultProps = {
